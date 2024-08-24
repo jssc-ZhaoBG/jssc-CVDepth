833eda817aaa65ffe65554ac6f530d92b1f4b36e
zhao bai gan <zhaobaigan@126.com>
2024-08-24 20:16:57 +0800

    Update __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -2,4 +2,3 @@
 from .kitti_dataset import KITTIRAWDataset, KITTIOdomDataset, KITTIDepthDataset
 from .cityscapes_preprocessed_dataset import CityscapesPreprocessedDataset
 from .cityscapes_evaldataset2 import CityscapesEvalDataset
-from .ucl_dataset import CustomUCLRAWDataset
